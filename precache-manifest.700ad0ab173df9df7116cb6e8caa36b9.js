526ac61e1b4b9c6be42e90266da4ee57a8fad976
Jerome Cortez <crtzjerome@gmail.com>
2020-05-01 21:12:15 +0800

    Updates

---
+++ precache-manifest.700ad0ab173df9df7116cb6e8caa36b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "435d7cb9aed21fac066eb2531a382dff",
+    "url": "/jeromify/index.html"
+  },
+  {
+    "revision": "cb6f11594ef983805039",
+    "url": "/jeromify/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7c0e524f96b08ec21213",
+    "url": "/jeromify/static/js/2.0a8b876c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jeromify/static/js/2.0a8b876c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb6f11594ef983805039",
+    "url": "/jeromify/static/js/main.c46a6128.chunk.js"
+  },
+  {
+    "revision": "04933da11c5b0e61682a",
+    "url": "/jeromify/static/js/runtime-main.b064a46a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jeromify/static/media/logo.5d5d9eef.svg"
+  }
+]);
